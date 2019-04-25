9c65ba54aaa07d5cebb1922b46aca4304449cb7e
ZeroTwoWaifu <tobias2002@live.nl>
2019-04-26 00:24:56 +0200

    Full game
    uploaded full game

---
+++ Survival/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+
+    public LayerMask MovementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, MovementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                // Move player to hit
+
+                //Stop focusing current focus
+
+            }
+        }
+	}
+
+}
---
+++ Survival/Assets/Scripts/Controllers/TimeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimeController : MonoBehaviour {
+
+    public float timer = 0;
+
+
+
+
+    public GameObject player1;
+    public GameObject deathObject;
+    public GameObject deathObject2;
+    public GameObject deathObject3;
+    public GameObject deathObject4;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        Debug.Log(timer);
+
+ 
+
+        if(timer >= 60 && player1 == true)
+        {
+            Destroy(deathObject);
+            
+        }
+
+    }
+
+
+
+}
